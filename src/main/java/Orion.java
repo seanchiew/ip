d09c9eda0da0eaeab4e1b7dda7bf912d22191571
Sean <166712476+seanchiew@users.noreply.github.com>
2026-01-22 02:39:34 +0800

    Level-1: Add echo and bye command

--- src/main/java/Orion.java
+++ src/main/java/Orion.java
@@ -1,11 +1,32 @@
+import java.util.Scanner;
+
 public class Orion {
-    private static final String LINE = "____________________________________________________________";
+    private static final String LINE = "    ____________________________________________________________";
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Greet
         System.out.println(LINE);
-        System.out.println("Hello! I'm Orion");
-        System.out.println("What can I do for you?");
+        System.out.println("    Hello! I'm Orion");
+        System.out.println("    What can I do for you?");
         System.out.println(LINE);
-        System.out.println("Bye. Hope to see you again soon!");
+
+        // Echo user input
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("bye")) {
+                break;
+            }
+            System.out.println(LINE);
+            System.out.println("    " + userInput);
+            System.out.println(LINE);
+        }
+
+        // Exit
         System.out.println(LINE);
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+        scanner.close();
     }
 }
