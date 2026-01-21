4d09b533f6c45be7e39bc2e904cb6dcccff89024
Sean <166712476+seanchiew@users.noreply.github.com>
2026-01-22 02:27:58 +0800

    Level-0: Implement greeting and exit

---
+++ src/main/java/Orion.java
@@ -0,0 +1,11 @@
+public class Orion {
+    private static final String LINE = "____________________________________________________________";
+    public static void main(String[] args) {
+        System.out.println(LINE);
+        System.out.println("Hello! I'm Orion");
+        System.out.println("What can I do for you?");
+        System.out.println(LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+    }
+}
